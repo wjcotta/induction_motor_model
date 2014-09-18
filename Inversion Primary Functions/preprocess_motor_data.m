837f5df6e9d7eb6e1c76b1715b336382adeb0fc6
Will Cotta <wjcotta@mit.edu>
2014-09-18 16:42:26 -0400

    remapped current and voltage inputs after e stop button install

--- Inversion Primary Functions/preprocess_motor_data.m
+++ Inversion Primary Functions/preprocess_motor_data.m
@@ -1,6 +1,6 @@
 function out = preprocess_motor_data(filename, fs, l2l_flag, P, encoder_count, mill)
 
-% This function converts the raw output of the Labjack DAQ device into physical units suitable for calculation, does time-shifting to account for interchannel delay, 
+% This function converts the raw output of the Labjack DAQ device into physical units suitable for calculation, does time-shifting to account for interchannel delay,
 % performs Clark and Park transformations (accounting for variable electric frequency over course of experiment), and produces an estimate of instantaneous electric frequency.
 
 if (nargin < 5)
@@ -12,7 +12,7 @@ load(filename)
 out.filename = filename;
 % L = length(data);
 
-%% Set calibration constants for the three-phase HP power supply and analyzer. 
+%% Set calibration constants for the three-phase HP power supply and analyzer.
 % Schantz box configured so all voltage channels measure the same voltage phase, likewise for the current channels.
 
 Slopes = [0.000935547853831 0.001010673290357 0.001015083089409 0.091020193288373 0.091089882503841 0.090792761167529];
@@ -28,19 +28,19 @@ Slopes = [0.000935547853831 0.001010673290357 0.001015083089409 0.09102019328837
 %     Slopes = [0.00101010738971818, 0.00101176539509996, 0.00100682083611568, 0.0914369606121276,  0.0913493710967854,  0.0916521868611356];
 %     % These are the mill box constants.
 % else
-%     Slopes = [0.00101139,  0.00101397, 0.0010135, 1, 1, 1];  
+%     Slopes = [0.00101139,  0.00101397, 0.0010135, 1, 1, 1];
 %     % Scaling here for AIN0-AIN2 different than above due to inline AA filter.
 % end
 % }
 
-%% Filter data to remove stator bar harmonics. 
- 
+%% Filter data to remove stator bar harmonics.
+
 Wp = 1200/(fs/2);                   % Passband edge frequency
 Ws = 1600/(fs/2);                   % Stopband edge frequency
-[n, Wn] = buttord(Wp, Ws, 1, 10);   % Calculates the minimum order and cutoff frequency of a Butterworth filter required to meet desired specifications. 
-[b, a] = butter(n, Wn);             % Returns filter numberator and denominator coefficients in vectors b and a, respectively. 
+[n, Wn] = buttord(Wp, Ws, 1, 10);   % Calculates the minimum order and cutoff frequency of a Butterworth filter required to meet desired specifications.
+[b, a] = butter(n, Wn);             % Returns filter numberator and denominator coefficients in vectors b and a, respectively.
 
-% Use outputs from butter function to filter the data. 
+% Use outputs from butter function to filter the data.
 data(:,1) = filtfilt(b,a,data(:,1));
 data(:,2) = filtfilt(b,a,data(:,2));
 data(:,3) = filtfilt(b,a,data(:,3));
@@ -48,9 +48,9 @@ data(:,4) = filtfilt(b,a,data(:,4));
 data(:,5) = filtfilt(b,a,data(:,5));
 data(:,6) = filtfilt(b,a,data(:,6));
 
-[~, sI, eI] = cycle_mean(data(:,4));  
+[~, sI, eI] = cycle_mean(data(:,4));
 % Finds mean between first maximum and last minimum for a rough estimate of the cycle mean.
-% Simply taking mean of entire signal can be misleading if signal is not composed of integer number of periods. 
+% Simply taking mean of entire signal can be misleading if signal is not composed of integer number of periods.
 % This ensures we start at a rise and end at a fall
 % We now have periodic signal vectors in the long data, which will cause less edge effect with fft-based processing later.
 
@@ -67,8 +67,8 @@ AIN5 = data(sI:eI,6);   % Vca if line to line measurement
 
 % Speed measurememnt done by counting clock pulses at 48 mHz between 2 pairs of rising or falling edges from a single encoder line.
 % Thus calculated speed is most acurate midway between the edge pairs from which it is derived.
-% The labjack latches the time between edge pairs after each applicable edge, making the speed derived from the reported time 
-% always at least 1/5 an edge pair duration old and up to 1 1/2 edge-pair durations olds (depending on when the latch contents 
+% The labjack latches the time between edge pairs after each applicable edge, making the speed derived from the reported time
+% always at least 1/5 an edge pair duration old and up to 1 1/2 edge-pair durations olds (depending on when the latch contents
 % are read by the data logger), for an average of 1 edge-pair duration time delay in the derived speed.
 
 if size(data,2) == 7
@@ -102,13 +102,13 @@ if size(data,2) == 11
 end
 
 if size(data,2) == 10
-    
+
     nom_Slope = 1.5629e-4;
     nom_Offset = -5.1760;
-       
+
     out.pu_volts_base = 190/sqrt(3);
     out.pu_current_base = 20.88;
-    
+
     out.TI_pwmdac_chan0 = ((nom_Slope.*data(sI:eI,7)) + nom_Offset-1.65)/1.65;
     out.TI_pwmdac_chan1 = ((nom_Slope.*data(sI:eI,8)) + nom_Offset-1.65)/1.65;
     out.TI_pwmdac_chan2 = ((nom_Slope.*data(sI:eI,9)) + nom_Offset-1.65)/1.65;
@@ -120,13 +120,13 @@ clear data
 
 %% Convert Labjack bits to currents
 
-Volts_LJ = @(Bits) (Bits/65536)*(5.07+5.18)-5.18; 
-I_measured = @(Bits) 1000*(Volts_LJ(Bits)/155); 
-% The resistance is set at 155 ohms using the switches in the nilm box (all resistors turned on). 
+Volts_LJ = @(Bits) (Bits/65536)*(5.07+5.18)-5.18;
+I_measured = @(Bits) 1000*(Volts_LJ(Bits)/155);
+% The resistance is set at 155 ohms using the switches in the nilm box (all resistors turned on).
 % 1000 is the conversion for the LA-55-P current transducer from measure current to output current.
 
 currents = [AIN0, AIN1, AIN2];
-    
+
 for i=1:3;
     for k=1:length(currents);
         temp(k,i)=I_measured(currents(k,i));
@@ -139,18 +139,18 @@ AIN2 = temp(:,3) - mean(temp(:,3));
 
 clear temp currents
 
-%% Use Slopes for Voltage 
+%% Use Slopes for Voltage
 
 AIN3 = (AIN3 - mean(AIN3))*Slopes(4);
 AIN4 = (AIN4 - mean(AIN4))*Slopes(5);
 AIN5 = (AIN5 - mean(AIN5))*Slopes(6);
 
-dt = 11.55/1e6;  
+dt = 11.55/1e6;
 % Best fit interchannel delay from test runs with 3 consecutive channels reading the same voltage phase.
 
 t_shift = @(a,t0,fs) ifft(fft(a).*exp(-1i*2*pi*t0*(ifftshift((0:length(a)-1)' -ceil((length(a)-1)/2))/length(a)*fs)));
 % fftshift and ifftshift switch the left and right half in order to put the zero frequency at the center.
-% Makes makes the first and last 50 points weird. 
+% Makes makes the first and last 50 points weird.
 
 theta_shift = @(a,theta) ifft(fft(a).*exp(-1i*theta*(ifftshift((0:length(a)-1)' -ceil((length(a)-1)/2)))));
 
@@ -161,7 +161,7 @@ AIN3 = t_shift(AIN3,3*dt,fs);
 AIN4 = t_shift(AIN4,4*dt,fs);
 AIN5 = t_shift(AIN5,5*dt,fs);
 
-if exist('Speed','var') % checks if variables are defined    
+if exist('Speed','var') % checks if variables are defined
     Speed = t_shift(Speed,dtS,fs);
 end
 
@@ -171,18 +171,18 @@ if l2l_flag %inverter connected wiring
     Ia = AIN0;
     Ib = AIN1;
     Ic = AIN2;
-    Vbc = real(AIN4); 
-    Vca = real(AIN5); 
+    Vbc = real(AIN4);
+    Vca = real(AIN5);
     Vab = real(AIN5);
-    
+
     % need to convert form Line to Line voltages to Line to Neutral Voltages
     % following laughman Thesis.
     delta2wye_model = [1 -1 0; 0 1 -1; -1 0 1; 1 1 1];
     Vdelta = [Vab Vbc Vca zeros(size(Vab))];
     Vwye = zeros(length(Vdelta),3);
-    
+
     delta2wye_Operator = (delta2wye_model'*delta2wye_model)\delta2wye_model';
-    
+
     for k = 1:length(Vdelta)
         Vwye(k,:) = (delta2wye_Operator*Vdelta(k,:)')';
     end
@@ -194,8 +194,8 @@ if l2l_flag %inverter connected wiring
 else
 %}
 
-%% Map Labjack inputs. 
-    
+%% Map Labjack inputs.
+
 if mill=='light duty';
     Ia = AIN0; % For early heavy duty data this is negative.
     Ib = AIN1;
@@ -204,9 +204,9 @@ if mill=='light duty';
     Vb = AIN3;
     Vc = AIN5;
 else
-    Ia = AIN1; % For early heavy duty data this is negative.
-    Ib = AIN0;
-    Ic = AIN2;
+    Ia = AIN0;
+    Ib = -AIN1; % current sensor are on backwards so this is negative
+    Ic = -AIN2; % current sensor are on backwards so this is negative
     Va = AIN3; % These are remapped due to configuration differences in the NILM box.
     Vb = AIN4;
     Vc = AIN5;
@@ -251,7 +251,7 @@ out.we_mean = pi/mean(diff_time);
 mixer = exp(-1i*out.we_mean*out.Time);
 V_mixed = out.V_clark.*mixer;
 
-% Line below loads precomputed FIR filter coefficients form file much faster than recomputing them each time.  
+% Line below loads precomputed FIR filter coefficients form file much faster than recomputing them each time.
 % Contents are in vector called 'b'.
 load('LP_FIR_filter_coeff.mat');
 N = length(b);
@@ -269,7 +269,7 @@ park_vect = exp(-1i*phase);
 Idq = out.I_clark.*park_vect; % Current output from the Park Transform
 
 %%%%%%%%%% DEBUGGING %%%%%%%%%%
-Idq = complex(-imag(Idq),real(Idq)); 
+Idq = complex(-imag(Idq),real(Idq));
 
 Ids_mean = mean(real(Idq));
 Iqs_mean = mean(imag(Idq));
